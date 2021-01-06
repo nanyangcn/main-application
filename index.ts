2af0ea39680ec11ddb6de44c9a3c5a926f60a10b
Yang Nan <nanyang@kth.se>
2021-01-06 18:42:38 +0200

    1.07

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
+import express from 'express';
 
-const main = () => {
-  const uuid = uuidv4();
+const app = express();
+const uuid = uuidv4();
 
-  const printId = (uuid: string) => {
-    console.log(uuid);
-  };
-
-  printId(uuid);
-  setInterval(() => printId(uuid), 5000);
+const resGeneration = () => `${new Date().toISOString()}: ${uuid}`;
+  
+const logPrint = () => {
+  console.log(resGeneration());
+  setInterval(() => console.log(resGeneration()), 5000);
 };
 
-main();
+const PORT = 3002;
+
+app.get('/', (_req, res) => {
+  res.send(resGeneration());
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+  logPrint();
+})
